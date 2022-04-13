e8a55e522a827e4803c15894cc9755ef2a81ac03
firaoldida <firaoldida1@gmail.com>
2022-04-13 12:32:21 +0300

    Added validation to routes

--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,16 @@ import Dashboard from "../views/Dashboard.vue";
 import Login from "../views/Login.vue";
 import Register from "../views/Register.vue";
 import DefaultLayout from "../components/DefaultLayout.vue";
-import Surveys from "../views/Surveys.vue"
+import Surveys from "../views/Surveys.vue";
+import store from "../store";
+import AuthLayout from "../components/AuthLayout.vue";
 
 const routes = [
     {
         path: '/',
         redirect: '/dashboard',
         component: DefaultLayout,
+        meta: { requiresAuth: true },
         children: [
             {
                 path: '/dashboard',
@@ -24,20 +27,39 @@ const routes = [
         ]
     },
     {
-        path: '/login',
-        name: 'Login',
-        component: Login
+        path: '/auth',
+        redirect: '/login',
+        name: 'Auth',
+        component: AuthLayout,
+        meta: { isGuest: true },
+        children: [
+            {
+                path: '/login',
+                name: 'Login',
+                component: Login
+            },
+            {
+                path: '/register',
+                name: 'Register',
+                component: Register
+            }
+        ]
     },
-    {
-        path: '/register',
-        name: 'Register',
-        component: Register
-    }
 ]
 
 const router = createRouter({
     history: createWebHistory(),
     routes
+})
+
+router.beforeEach((to, from, next) => {
+    if (to.meta.requiresAuth && !store.state.user.token) {
+        next({ name: 'Login' })
+    } else if (store.state.user.token && (to.meta.isGuest)) {
+        next({ name: 'Dashboard'});
+    } else {
+        next()
+    }
 })
 
 export default router;
