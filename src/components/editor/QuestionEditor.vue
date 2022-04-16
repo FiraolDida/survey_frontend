33fc3e05ccee31f1a24ea58b06d65593aa9e7630
firaoldida <firaoldida1@gmail.com>
2022-04-16 12:24:50 +0300

    Modified the size of delete button on option

--- src/components/editor/QuestionEditor.vue
+++ src/components/editor/QuestionEditor.vue
@@ -79,7 +79,7 @@
                 <input type="text" v-model="option.text" @change="dataChange"
                     class="w-full rounded-sm py-1 px-2 text-xs border border-gray-300 focus:border-indigo-500" />
                 <button type="button" @click="removeOption(option)" class="h-6 w-6 rounded-full flex items-center justify-center border border-transparent transition-colors hover:border-red-100">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3 text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                         <path stroke-linecap="round" stroke-linejoin="round" fill-rule="evenodd" clip-rule="evenodd" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                     </svg>
                 </button>
